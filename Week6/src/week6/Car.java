b31531b72beacc109f389093611db61310ddc40f
Solomon Pearman <4pears91@solent.ac.uk>
2019-10-29 15:31:18 +0000

    Created the car class and added the attributes and a constructor

---
+++ Week6/src/week6/Car.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week6;
+
+/**
+ *
+ * @author 4pears91
+ */
+public class Car {
+    private int carnumber;
+    private String carcolor;
+  
+    public Car (int carnumberIn, String carcolorIn)
+    {
+        carnumber = carnumberIn;
+        carcolor = carcolorIn;
+    }
+    
+            
+}
