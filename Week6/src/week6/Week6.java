152e478127b28c5c504c8d3b0e3c68000df3d869
Solomon Pearman <4pears91@solent.ac.uk>
2019-10-29 17:00:37 +0000

    Created the carpark object and added some cars to the car park

--- Week6/src/week6/Week6.java
+++ Week6/src/week6/Week6.java
@@ -14,8 +14,28 @@ public class Week6 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    public static void main(String[] args) 
+    {
+        int state = 1;
+        
+            CarPark c1 = new CarPark();
+        
+                c1.addCar( new Car(123,"Red") );
+                c1.addCar( new Car(231,"Purple") );
+                c1.addCar( new Car(321,"Green") );
+                c1.addCar( new Car(543,"White") );
+                c1.addCar( new Car(431,"Black") );
+                c1.addCar( new Car(133,"Grey") );
+
+        
+        
+        while(state == 1)
+        { 
+            
+            
+            
+        }
+    
+}
     
 }
