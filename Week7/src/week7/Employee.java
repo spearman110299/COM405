04a4f68cbb20bbf19a735e2ebe48859a636010cb
Solomon Pearman <4pears91@solent.ac.uk>
2019-11-05 16:23:09 +0000

    Created the method for booking a holiday for an employee

--- Week7/src/week7/Employee.java
+++ Week7/src/week7/Employee.java
@@ -10,5 +10,55 @@ package week7;
  * @author 4pears91
  */
 public class Employee {
+    protected String name;
+    protected int payrate;
+    protected int reqhours;
+    protected int entholidays;
+
+    public Employee(String nameIn, int payrateIn, int reqhoursIn, int entholidaysIn)
+    {
+        name = nameIn;
+        payrate = payrateIn;
+        reqhours = reqhoursIn;
+        entholidays = entholidaysIn;
+               
+        
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+    public int getPayRate()
+    {
+        return payrate;
+    }
+    public int getReqHours()
+    {
+        return reqhours;
+    }
+    public int getEntHolidays()
+    {
+        return entholidays;
+    }
+    
+    
+    public int BookHoliday(int reduceby)
+    {
+        
+        
+        if(entholidays-reduceby>0)
+        {
+            entholidays=entholidays-reduceby;
+            //entholidays -= reduceby;
+        }
+        else
+        {
+            System.out.println("This employee has no holidays left");
+        }
+        return 0;
+    }
+    
+    
     
 }
